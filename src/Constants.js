6ce8e62ecd1269251361d43f63b744c267a0a650
Pramod <pramodmithyantha@gmail.com>
2019-07-25 20:46:37 +0530

    Working Code

---
+++ src/Constants.js
@@ -0,0 +1,22 @@
+export default {
+  //Default value
+  default : 100,
+  count : 0,
+  beatsPerMeasure : 4,
+  bpm : "BPM",
+
+  //Button Values
+  start : "START",
+  stop : "STOP",
+
+  //CSS Classes
+  App : "App",
+  custom_range : "custom-range",
+  range : "range",
+  variant : "outlined",
+  color : "secondary",
+
+  //Slider Values
+  min : 60,
+  max : 240
+}
